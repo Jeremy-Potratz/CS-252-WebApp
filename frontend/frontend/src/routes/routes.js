da7b375d2f60d02da3e9a97df8663f14e9ade316
Jeremy-Potratz <jpotrat@purdue.edu>
2019-04-22 21:11:47 -0400

    creates home js pages for various viewing pages
    Cool guy

---
+++ frontend/frontend/src/routes/routes.js
@@ -0,0 +1,22 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME_WITH_JOB_ID = '/home/:jobid';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
+export const MATCHES = '/matches';
+export const MATCHES_WITH_JOBID = '/matches/:jobid';
+export const VIEW = '/view/:email';
+export const COMPANY_HOME = '/company/:email';
+export const JOB_POSTING_CREATOR = '/jobpostingcreator'
+export const EDIT_JOB = '/editjob/:jobid'
+export const HISTORY = '/history';
+export const HISTORY_WITH_JOB_ID = '/history/:jobid';
+export const CHAT = '/chat/:id';
+export const CHAT_WITH_JOB_ID = '/chat/:id/from/:jobid';
+export const FAVORITE = '/favorite';
+export const VIEWJOBS = '/jobs/:jobid';
+
+export const BASE_URL = "http://localhost:3000"
